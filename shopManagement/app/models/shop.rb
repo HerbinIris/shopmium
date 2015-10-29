b3bbf0868094cbb72e85846cd4ecc59d681326c6
herbin <herbin.iris@gmail.com>
2015-10-30 00:42:40 +0900

    First filter change in model

--- shopManagement/app/models/shop.rb
+++ shopManagement/app/models/shop.rb
@@ -1,2 +1,37 @@
 class Shop < ActiveRecord::Base
+
+  filterrific(
+     default_filter_params: { sorted_by: 'name_asc' },
+     available_filters: [
+       :sorted_by,
+       :search_query,    
+     ]
+  )
+
+
+  scope :search_query, -> (name) { where("name like ?", "#{name}%")}
+
+
+
+  scope :sorted_by, lambda { |sort_option|
+    direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
+    case sort_option.to_s
+    when /^created_at_/
+      order("shops.created_at #{ direction }")
+    when /^name_/
+      order("shops.name #{ direction }")
+    else
+      raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect }")
+    end
+  }
+
+  def self.options_for_sorted_by
+    [
+      ['Name (a-z)', 'name_asc'],
+      ['Creation', 'created_at_asc'],
+    ]
+  end
+
 end
+
+
